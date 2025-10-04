630a64546eed4aed26ef62875028abfef74b4fbf
gimeast <slasnrndu@gmail.com>
2025-10-04 11:32:56 +0900

    client
    - 각각의 아이콘에 NavLink 사용

--- ootd-client/src/components/layout/BottomNav.tsx
+++ ootd-client/src/components/layout/BottomNav.tsx
@@ -4,38 +4,27 @@ import HomeIcon from '../icons/HomeIcon.tsx';
 import SearchIcon from '../icons/SearchIcon.tsx';
 import LikeIcon from '../icons/HeartIcon.tsx';
 import UserIcon from '../icons/UserIcon.tsx';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const BottomNav = () => {
-    const { pathname } = useLocation();
-    const navigate = useNavigate();
-
     const isLoggedIn = false; //todo: 실제 로그인 됐는지 체크하는 로직으로 수정 필요
-
-    const handleIcon = (path: string): void => {
-        navigate(`${path}`);
-    };
 
     return (
         <nav>
             <div className={styles.nav_inner_box}>
-                <HomeIcon className={pathname === '/' ? '' : styles.homeIcon} onClick={() => handleIcon('/')} />
-                <SearchIcon
-                    className={pathname === '/search' ? '' : styles.searchIcon}
-                    onClick={() => handleIcon('/search')}
-                />
-                <OotdAddIcon
-                    className={pathname === '/ootd/add' ? '' : styles.ootdAddIcon}
-                    onClick={() => handleIcon('/ootd/add')}
-                />
-                <LikeIcon
-                    className={pathname === '/likes' ? '' : styles.likeIcon}
-                    onClick={() => handleIcon('/likes')}
-                />
-                <UserIcon
-                    className={pathname === (isLoggedIn ? '/mypage' : '/login') ? '' : styles.userIcon}
-                    onClick={() => handleIcon(isLoggedIn ? '/mypage' : '/login')}
-                />
+                <NavLink to='/'>{({ isActive }) => <HomeIcon className={isActive ? '' : styles.homeIcon} />}</NavLink>
+                <NavLink to='/search'>
+                    {({ isActive }) => <SearchIcon className={isActive ? '' : styles.searchIcon} />}
+                </NavLink>
+                <NavLink to='/ootd/add'>
+                    {({ isActive }) => <OotdAddIcon className={isActive ? '' : styles.ootdAddIcon} />}
+                </NavLink>
+                <NavLink to='/likes'>
+                    {({ isActive }) => <LikeIcon className={isActive ? '' : styles.likeIcon} />}
+                </NavLink>
+                <NavLink to={isLoggedIn ? '/mypage' : '/login'}>
+                    {({ isActive }) => <UserIcon className={isActive ? '' : styles.userIcon} />}
+                </NavLink>
             </div>
         </nav>
     );
