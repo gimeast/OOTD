8bbe4aabb66d27d4b24cbafde30399dde9d70697
gimeast <slasnrndu@gmail.com>
2025-10-16 10:36:51 +0900

    client
    - ootd 목록조회 정렬필터 추가

--- ootd-client/src/pages/home/Home.tsx
+++ ootd-client/src/pages/home/Home.tsx
@@ -5,20 +5,27 @@ import type { PageResponseType } from '../../types/common.ts';
 import type { OotdItemType } from '../../types/ootd.ts';
 import { useScrollObserver } from '../../hooks/useScrollObserver.ts';
 import OotdItem from './OotdItem.tsx';
+import { useState } from 'react';
 
 const Home = () => {
+    const [sort, setSort] = useState<'id' | 'like'>('id');
+
     const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
-        queryKey: ['ootd', 'list'],
+        queryKey: ['ootd', 'list', sort],
         queryFn: ({ pageParam }: { pageParam: number }): Promise<PageResponseType<OotdItemType>> =>
             apiClient(API_ENDPOINTS.OOTD.LIST, {
                 method: 'GET',
-                params: { page: pageParam },
+                params: { page: pageParam, sort },
             }),
         getNextPageParam: (lastPage: PageResponseType<OotdItemType>, allPages) => {
             return lastPage.last ? undefined : allPages.length + 1;
         },
         initialPageParam: 1,
     });
+
+    const handleSort = (newSort: 'id' | 'like') => {
+        setSort(newSort);
+    };
 
     useScrollObserver(() => {
         if (hasNextPage && !isFetchingNextPage) {
@@ -29,8 +36,12 @@ const Home = () => {
     return (
         <div className={styles.home}>
             <div className={styles.filter_btn_group}>
-                <button className={styles.active}>최신순</button>
-                <button>인기순</button>
+                <button className={sort === 'id' ? styles.active : ''} onClick={() => handleSort('id')}>
+                    최신순
+                </button>
+                <button className={sort === 'like' ? styles.active : ''} onClick={() => handleSort('like')}>
+                    인기순
+                </button>
             </div>
 
             {data?.pages.map((page: PageResponseType<OotdItemType>) =>
