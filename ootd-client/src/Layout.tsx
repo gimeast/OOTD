1d4d11d5ec4b74ab30afb868cff4f5c88403d507
gimeast <slasnrndu@gmail.com>
2025-10-30 13:24:06 +0900

    client
    - 공통 모달 열렸을때 뒷배경 스크롤, 터치액션 안되도록 수정

--- ootd-client/src/Layout.tsx
+++ ootd-client/src/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet, useLocation } from 'react-router-dom';
 import Header from './components/layout/Header.tsx';
 import Footer from './components/layout/Footer.tsx';
 import BottomNav from './components/layout/BottomNav.tsx';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BasicModal from './components/common/modal/BasicModal.tsx';
 import useModalStore from './stores/useModalStore.ts';
 
@@ -14,6 +14,22 @@ const Layout = () => {
 
     const hideBottomNavPaths = ['/join'];
     const bottomNav = !hideBottomNavPaths.includes(pathname);
+
+    useEffect(() => {
+        if (isOpen) {
+            const scrollY = window.scrollY;
+            document.body.style.position = 'fixed';
+            document.body.style.top = `-${scrollY}px`;
+            document.body.style.width = '100%';
+
+            return () => {
+                document.body.style.position = '';
+                document.body.style.top = '';
+                document.body.style.width = '';
+                window.scrollTo(0, scrollY);
+            };
+        }
+    }, [isOpen]);
 
     return (
         <>
